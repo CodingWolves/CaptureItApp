06a901a917fb8e743a0e34e073e59056edee6d3a
GalZilca <galzilca@gmail.com>
2020-09-19 11:32:48 +0300

    take pic button with onClick

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,19 @@
 import React from 'react'
-// import '../styles/styles.scss'
+import { Link } from 'react-router-dom';
 
 import { Header } from './Header'
 
+const takePhoto = (e) => {
+    // if photo taken
+    document.getElementById("takePhoto").src = require("../images/me.png")
+}
+
 export const Dashboard = () => (
-    <Header />
+    <div>
+        <Header />
+
+        <button onClick={ takePhoto } style={{ marginLeft: '5rem' }}>
+            <img id="takePhoto" className="img-responsive" src={require('../images/add-photo.png')} />
+        </button>
+    </div>
 )
