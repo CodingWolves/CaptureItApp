2c823f3c0baa06c8439734d988efbf55e09f0c90
idozahavy <idozahavy@users.noreply.github.com>
2020-09-19 21:11:11 +0300

    camera capture

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Header } from './Header'
+import { Header } from "./Header";
 
 const takePhoto = (e) => {
-    // if photo taken
-    document.getElementById("takePhoto").src = require("../images/me.png")
+  console.log(navigator);
+  console.log(global);
+  var cameraOptions = {
+    sourceType: window.Camera.PictureSourceType.CAMERA,
+    correctOrientation: true,
+  };
+
+  navigator.camera.cleanup(); // removes the last image taken on ios
+  navigator.camera.getPicture(pictureSuccess, pictureError, cameraOptions);
+};
+
+function pictureSuccess(picture_path) {
+  if (window.cordova.platformId === "browser") {
+    document.getElementById("takePhoto").src =
+      "data:image/png;base64, " + picture_path;
+  } else {
+    document.getElementById("takePhoto").src = picture_path;
+  }
+}
+
+function pictureError(msg) {
+  alert(msg);
 }
 
 export const Dashboard = () => (
-    <div>
-        <Header />
+  <div>
+    <Header />
 
-        <button onClick={ takePhoto } style={{ marginLeft: '5rem' }}>
-            <img id="takePhoto" className="img-responsive" src={require('../images/add-photo.png')} />
-        </button>
-    </div>
-)
+    <button onClick={takePhoto} style={{ marginLeft: "5rem" }}>
+      <img
+        id="takePhoto"
+        className="img-responsive"
+        src={require("../images/add-photo.png")}
+      />
+    </button>
+  </div>
+);
