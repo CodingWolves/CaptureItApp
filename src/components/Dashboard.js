9589678878be36e59660f52cf484c08f3ec8e4ca
idozahavy <idozahavy@users.noreply.github.com>
2020-09-22 14:05:37 +0300

    dialog for camera or gallery
    #15
    In browser the dialog shows only Ok/Cancel and not what we want, for now it is set to take picture from gallery automaticaly on browser, @nadavWeisler need to add it to TODO list.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,17 +6,40 @@ import { Header } from "./Header";
 const takePhoto = (e) => {
   console.log(navigator);
   console.log(global);
+
+  if (window.cordova.platformId === "browser") {
+    // TODO special confirmation, notification not working good on browser
+    phoneConfirmCallback(2); // for now no confirmation for gallery
+  } else {
+    navigator.notification.confirm(
+      "Where do you want to take the picture from?",
+      phoneConfirmCallback,
+      "Picture",
+      ["Camera", "Gallery"] // the order is important for the callback function, up to 3 options on android
+    );
+  }
+};
+
+function phoneConfirmCallback(selected_index) {
+  // alert(JSON.stringify(clicked_index, null, 4));
   var cameraOptions = {
-    sourceType: window.Camera.PictureSourceType.CAMERA,
     correctOrientation: true,
   };
-
+  if (selected_index === 1) {
+    // selected Camera
+    cameraOptions.sourceType = window.Camera.PictureSourceType.CAMERA;
+  }
+  if (selected_index === 2) {
+    // selected Gallery
+    cameraOptions.sourceType = window.Camera.PictureSourceType.SAVEDPHOTOALBUM;
+  }
   navigator.camera.cleanup(); // removes the last image taken on ios
   navigator.camera.getPicture(pictureSuccess, pictureError, cameraOptions);
 };
 
 function pictureSuccess(picture_path) {
   if (window.cordova.platformId === "browser") {
+    // browser returns base64 image data instead of picture path
     document.getElementById("takePhoto").src =
       "data:image/png;base64, " + picture_path;
   } else {
