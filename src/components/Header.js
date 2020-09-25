31bf449fb3fd44702826a806cf28a65fb599ca20
GalZilca <galzilca@gmail.com>
2020-09-25 11:09:37 +0300

    UI design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export const Header = (props) => (
-    <h1 style={{ marginLeft: "5rem", color: "#483D8B" }} >{navigator.app_lang.page_header}</h1>
+    <h1 className="header" >{ avigator.app_lang.page_header }</h1>
 )
